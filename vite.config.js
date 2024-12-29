c6fe9ae0ab35a4966c7a592124d0385d99a75f26
keserizzet <izzetfurkankeserwtk@gmail.com>
2024-12-30 00:04:08 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '/goit-js-hw-09/', // Eğer HTML dosyanız src klasöründeyse
-  // build: {
-  //   outDir: 'dist', // Çıktı dizini
-  // },
+  build: {
+    outDir: 'dist', // Çıktı dizini
+  },
 });
