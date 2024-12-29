51186b468af7fa13af387cf0eb974621b40773fd
keserizzet <izzetfurkankeserwtk@gmail.com>
2024-12-30 00:38:56 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,7 @@
 // window.global = window; 
 
 import SimpleLightbox from "./node_modules/simplelightbox/dist/simple-lightbox.esm.js"
-  import "./node_modules/simplelightbox/dist/simple-lightbox.min.css";
+  import "simplelightbox/dist/simple-lightbox.min.css";
   import { images as galleryImages } from '/goit-js-hw-09/src/data/images.js';
 
 
